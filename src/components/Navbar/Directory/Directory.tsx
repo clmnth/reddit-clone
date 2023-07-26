a5dd5c0d809f0a6206a7e5577f49458eac02af44
Clmt <huvigclement@gmail.com>
2023-07-26 16:18:26 +0200

    useDirectory cutsom react hook

--- src/components/Navbar/Directory/Directory.tsx
+++ src/components/Navbar/Directory/Directory.tsx
@@ -1,13 +1,24 @@
 import { authModalState } from "@/src/atoms/authModalAtom";
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Flex, Menu, MenuButton, MenuList, Icon, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Menu,
+  MenuButton,
+  MenuList,
+  Icon,
+  Text,
+  Image,
+} from "@chakra-ui/react";
 import React from "react";
 import { TiHome } from "react-icons/ti";
 import Communities from "./Communities";
+import useDirectory from "@/src/hooks/useDirectory";
 
 const Directory: React.FC = () => {
+  const { directoryState, toggleMenuOpen } = useDirectory();
+
   return (
-    <Menu>
+    <Menu isOpen={directoryState.isOpen}>
       <MenuButton
         cursor="pointer"
         padding="0px 6px"
@@ -18,6 +29,7 @@ const Directory: React.FC = () => {
           outline: "1px solid",
           outlineColor: "gray.200",
         }}
+        onClick={toggleMenuOpen}
       >
         <Flex
           align="center"
@@ -25,17 +37,34 @@ const Directory: React.FC = () => {
           width={{ base: "auto", lg: "200px" }}
         >
           <Flex align="center">
-            <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
+            {directoryState.selectedMenuItem.imageURL ? (
+              <Image
+                src={directoryState.selectedMenuItem.imageURL}
+                borderRadius="full"
+                boxSize="24px"
+                mr={2}
+              />
+            ) : (
+              <Icon
+                fontSize={24}
+                mr={{ base: 1, md: 2 }}
+                as={directoryState.selectedMenuItem.icon}
+                color={directoryState.selectedMenuItem.iconColor}
+              />
+            )}
+
             <Flex display={{ base: "none", lg: "flex" }}>
               <Text fontWeight={600} fontSize="10pt">
-                Home
+                {directoryState.selectedMenuItem.displayText}
               </Text>
             </Flex>
           </Flex>
           <ChevronDownIcon />
         </Flex>
       </MenuButton>
-      <MenuList><Communities /></MenuList>
+      <MenuList>
+        <Communities />
+      </MenuList>
     </Menu>
   );
 };
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -59,7 +59,7 @@ const Posts: React.FC<PostsProps> = ({ communityData }) => {
 
   useEffect(() => {
     getPosts();
-  }, []);
+  }, [communityData]);
 
   return (
     <>
