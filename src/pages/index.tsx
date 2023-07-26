b46ec1667099286c677b65a8dbff3b59c1d60ad0
Clmt <huvigclement@gmail.com>
2023-07-26 18:19:12 +0200

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
 import type { NextPage } from "next";
-
+import PageContent from "../components/layout/PageContent";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/clientApp";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
-  return <div>Hello World!</div>
+  const [user, loadingUser] = useAuthState(auth);
+
+  const buildUserHomeFeed = () => {};
+  const buildNonUserHomeFeed = () => {};
+  const getUserPostVotes = () => {};
+
+  // useEffects
+  useEffect(() => {
+    if (!user && !loadingUser) buildNonUserHomeFeed();
+
+  }, [user, loadingUser]);
+
+  return (
+    <PageContent>
+      <>{/* <PostFeed /> */}</>
+      <>{/* Recommendations */}</>
+    </PageContent>
+  );
 };
 
 export default Home;
